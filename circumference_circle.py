18d9642634c609bb0b2585acd2d109f83c91adf6
Ubuntu <ubuntu@ip-172-31-18-12.ec2.internal>
2019-09-19 18:35:02 +0000

    program and module added

---
+++ circumference_circle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+# Created by Amir Mersad
+# Created on September 2019
+# This program calculates circumference of a circle
+#     with user input
+
+import constants
+
+
+def main():
+    # this function calculates circumference
+
+    # input
+    radius = int(input("Enter radius of circle (mm): "))
+
+    # process
+    circumference = constants.TAU*radius
+
+    # output
+    print("")
+    print("Circumference is {}mm^2".format(circumference))
+
+
+if __name__ == "__main__":
+    main()
---
+++ constants.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# Created by Amir Mersad
+# Created on September 2019
+# This module contain constants
+
+TAU = 6.28
